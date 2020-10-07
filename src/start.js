0ebcd5c2629d41544bf74d92ef78c5fb79694128
Hakan Serce <hakanserce@gmail.com>
2020-10-07 02:12:24 -0700

    some progress in prototype
    - now we start with an empty model (with just a root)
    - we have some keybindings and test code in place

--- src/start.js
+++ src/start.js
@@ -1,62 +1,40 @@
 /*global require, document, window, console */
 const MAPJS = require('mindmup-mapjs'),
-	jQuery = require('jquery'),
-	themeProvider = require('./theme'),
-	ThemeProcessor = require('mindmup-mapjs-layout').ThemeProcessor,
-	testMap = require('./example-map'),
-	content = require('mindmup-mapjs-model').content,
-	init = function () {
-		'use strict';
-		const container = jQuery('#container'),
-			idea = content(testMap),
-			imageInsertController = new MAPJS.ImageInsertController('http://localhost:4999?u='),
-			mapModel = new MAPJS.MapModel(MAPJS.DOMRender.layoutCalculator, []);
+	  jQuery = require('jquery'),
+	  themeProvider = require('./theme'),
+	  ThemeProcessor = require('mindmup-mapjs-layout').ThemeProcessor,
+	  initialMap = require('./initial-map'),
+	  content = require('mindmup-mapjs-model').content,
+	  mapModel = new MAPJS.MapModel(MAPJS.DOMRender.layoutCalculator, []),
+	  init = function () {
+		  'use strict';
+		  const container = jQuery('#container'),
+				idea = content(initialMap);
 
-		jQuery.fn.attachmentEditorWidget = function (mapModel) {
-			return this.each(function () {
-				mapModel.addEventListener('attachmentOpened', function (nodeId, attachment) {
-					mapModel.setAttachment(
-							'attachmentEditorWidget',
-							nodeId, {
-								contentType: 'text/html',
-								content: window.prompt('attachment', attachment && attachment.content)
-							}
-							);
-				});
-			});
-		};
-		window.onerror = window.alert;
+		  window.onerror = window.alert;
 
 
-		jQuery('#themecss').themeCssWidget(themeProvider, new ThemeProcessor(), mapModel);
-		container.domMapWidget(console, mapModel, false, imageInsertController);
-		jQuery('body').mapToolbarWidget(mapModel);
-		jQuery('body').attachmentEditorWidget(mapModel);
-		mapModel.setIdea(idea);
+		  jQuery('#themecss').themeCssWidget(themeProvider, new ThemeProcessor(), mapModel);
+		  container.domMapWidget(console, mapModel, false);
+		  jQuery('body').mapToolbarWidget(mapModel);
+		  mapModel.setIdea(idea);
+
+		  jQuery('body').keypress(function(event){
+			  if (event.ctrlKey == true && event.which == 13) {
+				  mapModel.addSubIdea();
+			  } else if (event.ctrlKey == true && event.which == 37) { // left arrow
+				  mapModel.moveLeft();
+			  } else if (event.ctrlKey == true && event.which == 39) { // right arrow
+				  mapModel.moveRight();
+			  }
+		  })
 
 
-		jQuery('#linkEditWidget').linkEditWidget(mapModel);
-		window.mapModel = mapModel;
-		jQuery('.arrow').click(function () {
-			jQuery(this).toggleClass('active');
-		});
-		imageInsertController.addEventListener('imageInsertError', function (reason) {
-			console.log('image insert error', reason);
-		});
-		container.on('drop', function (e) {
-			const dataTransfer = e.originalEvent.dataTransfer;
-			e.stopPropagation();
-			e.preventDefault();
-			if (dataTransfer && dataTransfer.files && dataTransfer.files.length > 0) {
-				const fileInfo = dataTransfer.files[0];
-				if (/\.mup$/.test(fileInfo.name)) {
-					const oFReader = new window.FileReader();
-					oFReader.onload = function (oFREvent) {
-						mapModel.setIdea(content(JSON.parse(oFREvent.target.result)));
-					};
-					oFReader.readAsText(fileInfo, 'UTF-8');
-				}
-			}
-		});
-	};
+		  jQuery('#linkEditWidget').linkEditWidget(mapModel);
+		  window.mapModel = mapModel;
+		  jQuery('.arrow').click(function () {
+			  jQuery(this).toggleClass('active');
+		  });
+	  };
+
 document.addEventListener('DOMContentLoaded', init);
